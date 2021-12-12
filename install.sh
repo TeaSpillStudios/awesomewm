488a94c9e4032618e99b640a17d2b5a467055006
Benmac83 <57747851+Benmac83@users.noreply.github.com>
2021-12-12 21:38:26 +0000

    Better comments and better packagemanagement

--- install.sh
+++ install.sh
@@ -1,15 +1,34 @@
 #!/bin/bash
 
-packages=("picom" "nitrogen" "ttf-font-awesome" "awesome-terminal-fonts" "awesome")
+# --- Read contents of package file into array -----
 
+packageFile=packages
+
+declare -a packageArray
+
+mapfile -t packageArray < ./packages              
+
+# --------------------------------------------------
+
+# --- Function to install packages without prompt -- 
 installPackage() {
-  sudo pacman -S $1 --noconfirm 
+  yes | sudo pacman -S $1 --noconfirm 
 }
+
+# -------------------------------------------------- 
+
+
+# --- Copy configs to config folder ----------------
 
 installConfig() {
   mkdir -p $HOME/.config/awesome/
   cp awesome/* $HOME/.config/awesome -r
 }
+
+# --------------------------------------------------  
+
+
+# --- Replace variables in config file -------------
 
 customiseConfig() {
   read -p "Terminal: " terminal
@@ -19,15 +38,27 @@ customiseConfig() {
   sed -i -e "s/brave/$browser/g" $HOME/.config/awesome/rc.lua
 }
 
+# --------------------------------------------------
+
+
+# --- Setup the lain module to AwesomeWM -----------
+
 installLain() {
   git clone https://github.com/lcpz/lain $HOME/.config/awesome/lain/
 }
 
-for package in "${packages[@]}"
-do
-  installPackage $package
-done
+# --------------------------------------------------
 
-installConfig
-customiseConfig
-installLain
+
+main() {
+  for package in "${packageArray[@]}"
+  do
+    installPackage $package
+  done
+
+  installConfig
+  customiseConfig
+  installLain
+}
+
+main
